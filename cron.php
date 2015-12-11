0f5befdcbf317c5574d44f71cf043ddc70adb28d
Daria Chechulina <dashachechulina@gmail.com>
2015-12-11 16:57:23 +0600

    add cron script

---
+++ cron.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "HI";
+
+$login = "root";
+$passwd = "qwerty123";
+$dat = date("y.m.d");
+$tm = date("h:i:s");
+
+$conn= new PDO("mysql:host=localhost;dbname=testdb", $login, $passwd);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$query = $conn->prepare("SELECT * FROM tmplinks WHERE uid = 2");
+$query->execute();
+$result = $query->fetchAll();
+$l = count($result);
+if ($l != 0)
+{
+    $tm = date('h:i:s');
+    $new_hash = "hash" . $tm;
+   /* $query = $conn->prepare("UPDATE tmplinks SET hash='$new_hash' WHERE uid=2");
+    $query->execute();*/
+}
